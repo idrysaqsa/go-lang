0bbc87f9a8a3fe984c429d18482877be7180bf5f
Aqsa Idris <aqsaidris31@gmail.com>
2024-12-26 15:50:29 +0700

    update code

--- hello.go
+++ hello.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+);
+
+func sayHello() {
+	var name string = "Aqsa Idris";
+	var age int = 20;
+
+	fmt.Println("Hello ", name, " and I am ", age, " years old");
+};
+
 func main() {
-	fmt.Println("Hello World")
-}
+	fmt.Println("Hello World");
+	sayHello();
+};
